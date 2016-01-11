ec4b01745a8e9a12a2162b9e84e279ede2967d40
Wouter Vroege <woutervroege@MacbookProWV.home>
2016-01-11 14:53:09 +0100

    small fixes

--- app/controllers/locations.js
+++ app/controllers/locations.js
@@ -1,31 +1,37 @@
 Ti.App.addEventListener('app:paranoia', function(data) {
     var locationsData = data.paranoiaChange[0].locations;
-    updateParis(locationsData[0].change)
-    updateAmsterdam(locationsData[1].change)
-    updateBerlin(locationsData[2].change)
+    updateParis(locationsData)
+    updateAmsterdam(locationsData)
+    updateBerlin(locationsData)
 })
 
-function updateParis(change) {
-    var change = parseChangePercentage(change)
-    $.ParanoiaParis.text = ((change>0) ? "+" : "") + change + "%"
+function getChangeByLocationName(locationName, locationsData) {
+    var keys = Object.keys(locationsData)
+    for(var i in locationsData) {
+        if(locationsData[i].name == locationName) return locationsData[i].change
+    }
+}
+
+function updateParis(locationsData) {
+    var change = getChangeByLocationName('Paris', locationsData)
+    parsePercentageLabel($.ParanoiaParis, change)
     changeBackground(change, $.Paris)
 }
 
-function updateAmsterdam(change) {
-    var change = parseChangePercentage(change)
-    $.ParanoiaAmsterdam.text = ((change>0) ? "+" : "") + change + "%"
+function updateAmsterdam(locationsData) {
+    var change = getChangeByLocationName('Amsterdam', locationsData)
+    parsePercentageLabel($.ParanoiaAmsterdam, change)
     changeBackground(change, $.Amsterdam)
 }
 
-function updateBerlin(change) {
-    var change = parseChangePercentage(change)
-    $.ParanoiaBerlin.text = ((change>0) ? "+" : "") + change + "%"
+function updateBerlin(locationsData) {
+    var change = getChangeByLocationName('Berlin', locationsData)
+    parsePercentageLabel($.ParanoiaBerlin, change)
     changeBackground(change, $.Berlin)
 }
 
 function changeBackground(changePercentage, view) {
     var color = getGradientRgbColorByPercentage(changePercentage)
-    console.log('color', color)
     backgroundGradient = {
         type: 'linear',
         startPoint: { x: '0%', y: '0%' },
@@ -36,6 +42,7 @@ function changeBackground(changePercentage, view) {
 }
 
 function getGradientRgbColorByPercentage(changePercentage) {
+    console.log('changePercentage=' + changePercentage)
     if(changePercentage <= -50) return '254, 239, 252'
     if(changePercentage <= -25) return '255, 181, 243'
     if(changePercentage <= 0) return '150,170,255' 
@@ -44,8 +51,15 @@ function getGradientRgbColorByPercentage(changePercentage) {
     return '255, 0, 0'
 }
 
+function parsePercentageLabel(label, newValue) {
+    var currentValue = parseInt(parseFloat(label.getText().replace(/(\%|\+)/g, "")).toFixed(1))
+    var newValue = parseFloat(newValue).toFixed(1)
+    return label.text = ((newValue>0) ? "+" : "") + newValue + "%"
+}
+
 function parseChangePercentage(change) {
-    return parseInt(change).toFixed(0)
+    var newChange = parseFloat(change).toFixed(1)
+    return newChange;
 }
 
 $.locations.addEventListener('open', function() {
